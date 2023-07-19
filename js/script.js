6f0257f04d8ac4688e7037f4f197ed708220a98b
Husseini <mhmd.husseinyy@gmail.com>
2023-07-19 14:00:50 +0300

    fixing total sum and delete functions

--- js/script.js
+++ js/script.js
@@ -1,37 +1,53 @@
-function expenseItemElement(text1,text2) {
-  return  `<tr>
+function expenseItemElement(text1, text2) {
+  return `<tr>
       <td class="text">${text1}</td>
       <td class="text2">${text2}</td>
       <td class="delete">&#128465</td>
-      </tr>`
+      </tr>`;
 }
+
 function addExpenseItem() {
   const expenseList = $(".expenseList");
   const input_name = $("#input_name");
   const input_expense = $("#input_expense");
 
   if (input_name.val().trim() === "" || input_expense.val().trim() === "") {
-      return;
+    return;
   }
-  const items = $(expenseItemElement(input_name.val(),input_expense.val()));
 
+  const name = input_name.val().trim();
+  const amount = parseFloat(input_expense.val().trim());
+
+  if (isNaN(amount) || amount <= 0) {
+    alert("Please enter a valid expense amount.");
+    return;
+  }
+
+  const items = $(expenseItemElement(name, `$${amount.toFixed(2)}`));
   expenseList.append(items);
 
   input_name.val("");
   input_expense.val("");
-   let total_sum = 0;
-$(".expenseList .text2 ").each(function () {
-  total_sum += parseInt($(this).text());
-});
-  items.find(".delete").click(function() {
-      let deletedExpense = parseInt(items.find(".text2 ").text());
-      total_sum -= deletedExpense;
-      items.remove();
-  })   
-  console.log(total_sum); 
+  updateTotalSum();
 }
 
-$(document).ready(function() {
+function updateTotalSum() {
+  let total_sum = 0;
+  $(".expenseList .text2").each(function () {
+    const expenseAmount = parseFloat($(this).text().slice(1));
+    total_sum += expenseAmount;
+  });
+  $("#totalSum").text(`$${total_sum.toFixed(2)}`);
+}
+
+$(document).ready(function () {
   const addBtn = $("#add_btn");
   addBtn.click(addExpenseItem);
-})
+
+  $(".expenseList").on("click", ".delete", function () {
+    const expenseRow = $(this).closest("tr");
+    const deletedExpense = parseFloat(expenseRow.find(".text2").text().slice(1));
+    expenseRow.remove();
+    updateTotalSum();
+  });
+});
